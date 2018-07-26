0609902f701e15c57a316acc6ce758b50f18df74
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2018-07-26 08:40:57 +0200

    Test recipe revised
    - Test package is only verifying that the library could be liked with an
    executable.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -15,10 +15,6 @@ class Tinyxml2TestConan(ConanFile):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
         self.copy('*.so*', dst='bin', src='lib')
-        # Not very elegant solution:
-        # need to copy from the folder that contains this conan file to bin folder to be able
-        # to execute tests
-        self.copy("resources/*", src=os.path.join(os.getcwd(), "..", ".."), dst="bin")
 
     def test(self):
         if not tools.cross_building(self.settings):
